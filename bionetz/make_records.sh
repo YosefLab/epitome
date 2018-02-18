b1b66d9bf19c79cc1c78d258700d2fd771dfcdfc
J Weston Hughes <jwhughes@berkeley.edu>
2018-02-18 15:31:51 -0800

    Clean up code

--- bionetz/make_records.sh
+++ bionetz/make_records.sh
@@ -3,14 +3,17 @@
 
 let total=1000
 let numshards=$1
-for cell in 'HeLa-S3' 'GM12878' 'H1-hESC' 'HepG2' 'K562'
+# let features=$2
+# let out=$3
+# let data=$4
+for cell in 'HeLa-S3' # 'GM12878' 'H1-hESC' 'HepG2' 'K562'
 do
 	for i in `seq 1 $numshards`;
 	do
-		let step=$total/$numshards
+		let step=$total/$1
 		let start=(i-1)*$step
 		let stop=i*$step
-		python3 make_tf_records.py --start $start --stop $stop --cell $cell &
+		python3 make_tf_records.py --start $start --stop $stop --cell $cell --features $2 --out $3 --data $4 &
 		echo $cell $i
 	done
 	wait
--- bionetz/make_tf_records.py
+++ bionetz/make_tf_records.py
@@ -41,12 +41,13 @@ tfs = ['p300', 'NRSF', 'CTCF', 'GABP', 'JunD', 'CEBPB', 'Pol2', 'EZH2',
              'c-Myc','CHD2']
 
 # Generates records for a given cell type and range
-def records_iterator(input_, target, cell, start, stop):
+def records_iterator(input_, target, cell, start, stop, features_path,
+    ):
+
     # Builds dicts of indicies of different features
-    dnase_dict, tf_dict = parse_feature_name(
-    "../../DeepSEA-v0.94/resources/feature_name")
+    dnase_dict, tf_dict = parse_feature_name(features_path)
     
-    # builds the vector of locations, and the mask
+    # builds the vector of locations for querying from matrix, and the mask
     tf_vec = []
     i = 0
     for tf in tfs:
@@ -54,30 +55,36 @@ def records_iterator(input_, target, cell, start, stop):
             tf_vec += [(tf_dict[cell][tf], 1)]
         else:
             tf_vec += [(0, 0)]
-    tf_vec.sort()
-    tf_locs = np.array([i[0] for i in tf_vec])
-    tf_mask = np.array([i[1] for i in tf_vec])
+    tf_locs = np.array([v[0] for v in tf_vec])
+    tf_mask = np.array([v[1] for v in tf_vec])
 
-    num_samples = input_.shape[2]
-
-    # Pre-build these
+    # Pre-build these features
     mask_feature = iio.make_int64_feature(tf_mask)
     name_feature = iio.make_bytes_feature([bytes(cell, 'utf-8')])
 
+    num_samples = input_.shape[2]
     for i in range(start, min(stop, num_samples)):
 
+        # x is 1000 * 5, where the last row is all 0s or 1s depending on the
+        # dnase label
         x1 = input_[0:1000,:,i]
         x2 = np.array([[target[dnase_dict[cell], i]]] * 1000)
         x = np.append(x1, x2, 1)
 
-        y = target[tf_locs, i] * tf_mask
+        # y is queried from the target matrix. We mask by whether we actually
+        # have this TF for this cell type.
+        y = np.array([target[c, i] for c in tf_locs]) * tf_mask
 
+        # The features going into the example.
         features = {}
+        # x has to be flattened.
         features['x/data'] = iio.make_int64_feature(x.flatten())
         features['x/shape'] = iio.make_int64_feature(x.shape)
         features['y'] = iio.make_int64_feature(y)
+        # We'll use mask in the loss function. 
         features['mask'] = mask_feature
         features['cell'] = name_feature
+
         yield iio.make_example(features)
         
 def main():
@@ -85,20 +92,32 @@ def main():
     parser.add_argument('--start', default=0)
     parser.add_argument('--stop', default=1000)
     parser.add_argument('--cell', default='HeLa-S3')
+    # On millennium, these paths should not be in the home directory!!!!
+    parser.add_argument('--features', 
+        default='../../DeepSEA-v0.94/resources/feature_name')
+    parser.add_argument('--out', default='./output')
+    parser.add_argument('--data', default='../../deepsea_train/train.mat')
     args = parser.parse_args()
 
-    tmp = h5py.File('../../deepsea_train/train.mat')
-    i, t = tmp['trainxdata'], tmp['traindata']
-    iterator = records_iterator(i, t, args.cell, int(args.start), int(args.stop))
 
+    output_dir = os.path.join(args.out, args.cell)
+    if not glob.glob(args.out):
+        os.mkdir(args.out)
+    if not glob.glob(output_dir):
+        os.mkdir(output_dir)
+
+
+    tmp = h5py.File(args.data)
+    i, t = tmp['trainxdata'], tmp['traindata']
+
+    iterator = records_iterator(i, t, args.cell, int(args.start),
+     int(args.stop), features_path=args.features)
     compression_options = tf.python_io.TFRecordOptions(
         tf.python_io.TFRecordCompressionType.GZIP)
-    if not glob.glob('./output/'+args.cell): 
-        os.mkdir('./output/'+args.cell)
 
     iio.write_tfrecord(
         protos=iterator,
-        path=os.path.join('./output/'+args.cell, args.start),
+        path=os.path.join(output_dir, args.start),
         options=compression_options)
 
 
