7c5d38f611d37bf06089e949bae558f55604e8a6
J Weston Hughes <jwhughes@berkeley.edu>
2018-02-22 16:33:22 -0800

    Move tf_record writing scripts

---
+++ tf_records/make_records.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+let total=4400000
+let numshards=$1
+# let features=$2
+# let out=$3
+# let data=$4
+for cell in 'A549' # 'HeLa-S3'  'GM12878' 'H1-hESC' 'HepG2' 'K562'
+do
+	for i in `seq 1 $numshards`;
+	do
+		let step=$total/$1
+		let start=(i-1)*$step
+		let stop=i*$step
+		python3 make_tf_records.py --start $start --stop $stop --cell $cell --features $2 --out $3 --data $4 &
+		echo $cell $i
+	done
+	wait
+done
