30da084c6703d32a0e964d60b5e8e6e918c80e57
J Weston Hughes <jwhughes@berkeley.edu>
2018-03-13 17:52:00 -0700

    Add deepsea problem

--- tensor2tensor/problems.py
+++ tensor2tensor/problems.py
@@ -5,11 +5,15 @@ from __future__ import print_function
 import math
 import multiprocessing as mp
 import os
+import zipfile
+
 
 # Dependency imports
 
 import h5py
 import numpy as np
+from scipy.io import loadmat
+
 
 from tensor2tensor.data_generators import dna_encoder
 from tensor2tensor.data_generators import generator_utils
@@ -20,55 +24,8 @@ from tensor2tensor.utils import registry
 
 import tensorflow as tf
 
-_DEEPSEA_DOWNLOAD_URL = ("http://deepsea.princeton.edu/media/code/"
-	                     "deepsea_train_bundle.v0.9.tar.gz")
-_DEEPSEA_FILENAME = "deepsea_train_bundle.v0.9.tar.gz"
-_DEEPSEA_TRAIN_FILENAME = ""
-_DEEPSEA_TEST_FILENAME = ""
-_DEEPSEA_FEATURES_FILENAME = ""
-
-
-def _get_deepsea(directory):
-	"""Download all Omniglot files to directory unless they are there."""
-	generator_utils.maybe_download(
-		directory, _DEEPSEA_FILENAME, _DEEPSEA_DOWNLOAD_URL)
-
-	##################################################
-	# Your code here!
-	# Any aditional preprocessings such as unzipping
-	##################################################
-
-
-def _deepsea_train_generator(tmp_dir, cell, start, stop):
-	for i in range(100):
-
-		##################################################
-		# Your code here!
-		# Reading from an HDF5 file
-		##################################################
-
-		return inputs, targets
-
-
-def _deepsea_test_generator(tmp_dir, cell, start, stop):
-	for i in range(100):
-
-		##################################################
-		# Your code here!
-		# Reading from a MatLab file
-		##################################################
-
-		return inputs, outputs
-
-
-def _common_generator(tmp_dir, cell, start, stop, is_training):
-	if is_training:
-		return _deepsea_train_generator(tmp_dir, cell, start, stop)
-	else:
-		return _deepsea_test_generator(tmp_dir, cell, start, stop)
 		
-
-class TranscriptionFactorProblem(problem.Problem):
+class ProteinBindingProblem(problem.Problem):
 	"""Transcription factor binding site prediction."""
 
     @property
@@ -84,7 +41,7 @@ class TranscriptionFactorProblem(problem.Problem):
 	@property
 	def num_channels(self):
 		"""Input channels."""
-		return 5
+		return 4
 
 	@property
 	def num_classes(self):
@@ -96,58 +53,83 @@ class TranscriptionFactorProblem(problem.Problem):
 		"""Number of TF-Record shards."""
 		return 100
 
-	@property
-	def train_cells(self):
-		return ["HeLa-S3", "GM12878", "H1-hESC", "HepG2"]  # Fix me!
-
-	@property
-	def test_cells(self):
-		return ["K562"]  # Fix me!
-
-	@property
-	def train_start(self):
-		return 0  # Fix me!
-
-	@property
-	def train_stop(self):
-		return 1000  # Fix me!
-
-	@property
-	def test_start(self):
-		return 0  # Fix me!
-
-	@property
-	def test_stop(self):
-		return 1000  # Fix me!
-
 	def generator(self, tmp_dir, is_training):
-		_get_deepsea(tmp_dir)
+		raise NotImplementedError()
+		# _get_deepsea(tmp_dir)
 
-		cells = self.train_cells if is_training else self.test_cells
-		start = self.train_start if is_training else self.test_start
-		stop = self.train_stop if is_training else self.test_stop
-
-		for cell in cells:
-			for inputs, targets in _common_generator(
-				tmp_dir, cell, start, stop, is_training):
-				yield inputs, targets
+		# if is_training:
+		# 	return _deepsea_train_generator(tmp_dir, cell, start, stop)
+		# else:
+		# 	return _deepsea_test_generator(tmp_dir, cell, start, stop)
 
 	def generate_data(self, data_dir, tmp_dir, task_id=-1):
 		generator_utils.generate_dataset_and_shuffle(
 			self.generator(tmp_dir, is_training=True),
-			self.training_filepaths(data_dir, self.num_shards, shuffled=False),
+			self.training_filepaths(data_dir, self.num_shards, shuffled=True),
 			self.generator(tmp_dir, is_training=False),
-			self.dev_filepaths(data_dir, 1, shuffled=False))
+			self.dev_filepaths(data_dir, 1, shuffled=True))
 
 	def hparams(self, defaults, unused_model_hparams):
 		p = defaults
+		# TODO(alexyku): is it okay to use None here?
 		p.input_modality = {"inputs": (registry.Modalities.GENERIC, None)}
-		p.target_modality = (registry.Modalities.SYMBOL, 2)
+		p.target_modality = (registry.Modalities.SYMBOL, self.num_classes)
 		p.input_space_id = problem.SpaceID.GENERIC
 		p.target_space_id = problem.SpaceID.GENERIC
 
-	def example_reading_spec(self):
-		# Do not modify! Parse bytestrings in preprocess_example.
+
+class DeepSeaProblem(ProteinBindingProblem):
+	"""Transcription factor binding site prediction."""
+	_DEEPSEA_DOWNLOAD_URL = ("http://deepsea.princeton.edu/media/code/"
+	                     "deepsea_train_bundle.v0.9.tar.gz")
+	_DEEPSEA_FILENAME = "deepsea_train_bundle.v0.9.tar.gz"
+	_DEEPSEA_DIRNAME = "deepsea_train"
+	_DEEPSEA_TRAIN_FILENAME = "deepsea_train/train.mat"
+	_DEEPSEA_TEST_FILENAME = "deepsea_train/valid.mat"
+	_DEEPSEA_FEATURES_FILENAME = ""
+
+	def generator(self, tmp_dir, is_training):
+		self._get_data(tmp_dir)
+
+		if is_training:
+			return self._train_generator(tmp_dir)
+		else:
+			return self._test_generator(tmp_dir)
+
+	def _get_data(self, directory):
+		"""Download all Deepsea files to directory unless they are there."""
+		zip_name = os.path.join(directory, self._DEEPSEA_FILENAME)
+		generator_utils.maybe_download(
+			directory, self._DEEPSEA_FILENAME, self._DEEPSEA_DOWNLOAD_URL)
+		zipfile.ZipFile(zip_name, "r").extractall(zip_name)
+
+	def _train_generator(self, tmp_dir):
+		tmp = h5py.File(os.path.join(tmp_dir, self._DEEPSEA_TRAIN_FILENAME))
+        all_inputs, all_targets = tmp['trainxdata'], tmp['traindata']
+
+        for i in range(all_inputs.shape[2]):
+        	inputs = all_inputs[:, :, i].transpose([2, 0, 1])
+        	targets = all_targets[:, i].transpose([1, 0])
+
+        	yield {
+        		"inputs": [inputs.astype(np.bool).tobytes()],
+        		"targets": [targets.astype(np.bool).tobytes()],
+        	}
+
+	def _test_generator(tmp_dir, cell, start, stop):
+        tmp = loadmat(path)
+        all_inputs, all_targets = tmp['validxdata'], tmp['validdata']
+
+        for i in range(all_inputs.shape[0]):
+        	inputs = all_inputs[i].transpose([0, 2, 1])
+        	targets = all_targets[i].transpose([1, 0])
+
+        	yield {
+        		"inputs": [inputs.astype(np.bool).tobytes()],
+        		"targets": [targets.astype(np.bool).tobytes()],
+        	}
+
+    def example_reading_spec(self):
 	    data_fields = {
 	        "inputs": tf.FixedLenFeature([], tf.string),
 	        "targets": tf.FixedLenFeature([], tf.string),
@@ -165,22 +147,11 @@ class TranscriptionFactorProblem(problem.Problem):
 	    targets_shape = [self.num_output_predictions, self.num_classes]
 
 	    # Parse the bytestring based on how you encoded it in common_generator
-	    inputs = tf.reshape(tf.decode_raw(inputs, tf.float32), inputs_shape)
+	    inputs = tf.reshape(tf.decode_raw(inputs, tf.bool), inputs_shape)
 	    targets = tf.reshape(tf.decode_raw(targets, tf.bool), targets_shape)
-
-	    ################################################
-	    # Your code here!  Any aditional preprocessing
-	    # For example, converting to floats and ints.
-	    ################################################
 	    
 	    example["inputs"] = tf.to_float(inputs)
 	    example["targets"] = tf.to_int32(targets)
 
     	return example
 
-	def eval_metrics(self):
-		################################################
-		# Get the metrics you want to use.
-		# For example, we might want AUC, FPR, TPR, etc.
-		################################################
-    	return [metrics.Metrics.LOG_POISSON, metrics.Metrics.R2]
