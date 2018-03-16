8588aadc2ec44702a250f45b52eec9d01ea53ed5
Gunjan Baid <gunjan_baid@berkeley.edu>
2018-03-16 13:47:35 -0700

    Pushing version 2 of t2t code.

---
+++ tensor2tensor/run_v2.bash
@@ -0,0 +1,11 @@
+USR_DIR=v2_tensor2tensor
+PROBLEM=deepsea
+DATA_DIR=tensor2tensor/t2t_data
+TMP_DIR=/Users/Gunjan/Documents/BDG/epitome
+mkdir -p $DATA_DIR $TMP_DIR
+
+t2t-datagen \
+  --t2t_usr_dir=$USR_DIR \
+  --data_dir=$DATA_DIR \
+  --tmp_dir=$TMP_DIR \
+  --problem=$PROBLEM
