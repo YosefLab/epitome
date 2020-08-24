711a39b0696e41db85521b2f73fce9c73ca8cc88
akmorrow13 <akmorrow@berkeley.edu>
2020-08-24 10:41:43 -0500

    fixed tests to access test data

--- epitome/test/functions_test.py
+++ epitome/test/functions_test.py
@@ -15,19 +15,19 @@ def test_download_and_unzip(tmp_dir):
 
     files = os.listdir(os.path.join(dirname,"data"))
     assert(len(files) == 3)
-    
-    
-    
+
+
+
 def test_get_assays_single_assay():
     TF = 'JUND'
 
-    matrix, cellmap, assaymap = get_assays_from_feature_file(eligible_assays = TF, 
-                                                             min_cells_per_assay = 2, 
-                                                             min_assays_per_cell =2)
-    
+    matrix, cellmap, assaymap = get_assays_from_feature_file(feature_name_file = 'epitome/test/data/feature_name',
+            eligible_assays = TF,
+            min_cells_per_assay = 2,
+            min_assays_per_cell = 2)
+
     assays = list(assaymap)
     # Make sure only JUND and DNase are in list of assays
     assert(len(assays)) == 2
     assert(TF in assays)
     assert('DNase' in assays)
-           
