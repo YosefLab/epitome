96699d04b6cd47a560244c6aa03983558c214a0c
akmorrow13 <akmorrow@berkeley.edu>
2020-08-31 15:19:57 -0500

    unit tests download test data from S3

--- epitome/test/functions_test.py
+++ epitome/test/functions_test.py
@@ -1,49 +1,40 @@
+from epitome.test import EpitomeTestCase
 from epitome.test import *
 from epitome.functions import *
 import pytest
 import warnings
 
-@pytest.fixture
-def tmp_dir(tmpdir_factory):
-    return tmpdir_factory.mktemp("data")
-        
-def test_user_data_path():
-    # user data path should be able to be explicitly set
-    datapath = GET_DATA_PATH()
-    assert(datapath == os.environ["EPITOME_DATA_PATH"])
 
-def test_download_and_unzip(tmp_dir):
-    dirname = os.path.join(tmp_dir.dirname, "sub_dir")
-    assert(not os.path.exists(dirname))
+class FunctionsTest(EpitomeTestCase):
 
-    download_and_unzip(S3_TEST_PATH, dirname)
+    def __init__(self, *args, **kwargs):
+        super(EpitomeTestCase, self).__init__(*args, **kwargs)
 
-    files = os.listdir(os.path.join(dirname,"data"))
-    assert(len(files) == 3)
+    def test_user_data_path(self):
+        # user data path should be able to be explicitly set
+        datapath = GET_DATA_PATH()
+        assert(datapath == os.environ["EPITOME_DATA_PATH"])
 
+    def test_get_assays_single_assay(self):
+        TF = 'JUND'
 
-def test_get_assays_single_assay():
-    TF = 'JUND'
+        matrix, cellmap, assaymap = get_assays_from_feature_file(eligible_assays = TF,
+                min_cells_per_assay = 2,
+                min_assays_per_cell = 2)
 
-    matrix, cellmap, assaymap = get_assays_from_feature_file(eligible_assays = TF,
-            min_cells_per_assay = 2,
-            min_assays_per_cell = 2)
-
-    assays = list(assaymap)
-    # Make sure only JUND and DNase are in list of assays
-    assert(len(assays)) == 2
-    assert(TF in assays)
-    assert('DNase' in assays)
+        assays = list(assaymap)
+        # Make sure only JUND and DNase are in list of assays
+        assert(len(assays)) == 2
+        assert(TF in assays)
+        assert('DNase' in assays)
 
 
-def test_assays_SPI1_PAX5():
-    # https://github.com/YosefLab/epitome/issues/22
-    with warnings.catch_warnings(record=True) as warning_list:
-        warnings.simplefilter('always')
-        assays = list_assays()
-        matrix, cellmap, assaymap = get_assays_from_feature_file(eligible_assays = assays)
+    def test_assays_SPI1_PAX5(self):
+        # https://github.com/YosefLab/epitome/issues/22
+        with warnings.catch_warnings(record=True) as warning_list:
+            warnings.simplefilter('always')
+            assays = list_assays()
+            matrix, cellmap, assaymap = get_assays_from_feature_file(eligible_assays = assays)
 
-        assert(len(warning_list) == 2) # one for SPI1 and PAX5
-        assert(all(item.category == UserWarning for item in warning_list))
-        
-
+            assert(len(warning_list) == 2) # one for SPI1 and PAX5
+            assert(all(item.category == UserWarning for item in warning_list))
