2dfde40875fb6518bfda79e71e019d70fc804db2
Rohan Koodli <rovik05@gmail.com>
2020-09-02 13:33:29 -0700

    Data + test for score_peak_file (#33)
    *  Test for score_peak_file

--- epitome/test/models_test.py
+++ epitome/test/models_test.py
@@ -3,6 +3,8 @@ from epitome.constants import Dataset
 import numpy as np
 from epitome.models import VLP
 import pytest
+import tempfile
+import pyranges as pr
 
 class ModelsTest(EpitomeTestCase):
 
@@ -11,7 +13,30 @@ class ModelsTest(EpitomeTestCase):
 		self.model = self.makeSmallModel()
 		self.validation_size = 10
 
+	def test_score_peak_file(self):
+		test_similarity_peak_file = tempfile.NamedTemporaryFile(delete=False)
+		test_regions_peak_file = tempfile.NamedTemporaryFile(delete=False)
 
+		# Create dummy data
+		similarity_dict =  {'Chromosome': ['chr1', 'chr1', 'chr6'], 'Start': [200, 400, 1100],  'End': [220, 440, 1150]}
+		regions_dict = {'Chromosome': ['chr1', 'chr1'], 'Start': [210, 410],  'End': [215, 415]}
+		similarity_pr = pr.from_dict(similarity_dict)
+		regions_pr = pr.from_dict(regions_dict)
+
+		# Write to temp bed file
+		similarity_pr.to_bed(test_similarity_peak_file.name)
+		regions_pr.to_bed(test_regions_peak_file.name)
+
+		test_similarity_peak_file.flush()
+		test_regions_peak_file.flush()
+
+		preds = self.model.score_peak_file([test_similarity_peak_file.name], test_regions_peak_file.name, all_data=None)
+
+		test_regions_peak_file.close()
+		test_similarity_peak_file.close()
+
+		assert(preds.shape[0] == len(regions_pr))
+	
 	def test_train_model(self):
 		train_iters = 2
 
