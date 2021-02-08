4725a276903bb713f83b08cdb8cc904e4acd1a69
akmorrow13 <akmorrow@berkeley.edu>
2021-02-08 08:37:14 -0600

    added conversion

---
+++ docs/usage/create_dataset.rst
@@ -0,0 +1,118 @@
+Configuring data
+================
+
+Epitome pre-processes ChIP-seq peaks and DNase-seq peaks from ENCODE and ChIP-Atlas for usage
+in the Epitome models. Pre-processed datasets for hg19 are lazily downloaded from
+`Amazon S3 <https://epitome-data.s3-us-west-1.amazonaws.com/hg19/data.zip>`__
+when users run an Epitome model.
+
+
+Each downloaded dataset contains an h5 file (data.h5). This h5 file contains the following
+keys:
+
+- ``data``: a numerical matrix where rows indicate different assays and columns indicate genomic locations
+
+- ``rows``: row information for the data matrix.
+
+  - ``rows/celltypes``: which cell type corresponds to each row
+
+  - ``rows/targets``: which ChIP-seq target corresponds to each row. Can also be DNase-seq
+
+- ``columns``: contains information on the genomic locations that correspond to each
+
+  - ``columns/binSize``: size of genome regions (default is 200bp)
+
+  - ``columns/index/test_chrs``: test chromosomes (default is chrs 8/9)
+
+  - ``columns/index/valid_chrs``: validation chromosomes (default is chr 7)
+
+  - ``columns/index/TEST``: indices that specify the test set
+
+  - ``columns/index/VALID``: indices that specify the validation set
+
+  - ``columns/index/TRAIN``: indices that specify the train set (all autosomal chromosomes, excluding VALID and TEST
+
+  - ``columns/start``: start of each genomic location for each column
+
+  - ``columns/chr``: chromosome for each column
+
+- ``/meta``: metadata for how this dataset was generated
+
+  - ``meta/assembly``: genome assembly
+
+  - ``meta/source``: source for data. Either 'ChIP-Atlas' or 'ENCODE'
+
+Generating data for Epitome from ENCODE
+---------------------------------------
+
+You can generate your own Epitome dataset from ENCODE using the following command:
+``download_encode.py``.
+
+.. code:: bash
+
+  >> python download_encode.py -h
+
+    positional arguments:
+    download_path         Temporary path to download bed/bigbed files to.
+    {hg19,mm10,GRCh38}    assembly to filter files in metadata.tsv file by.
+    output_path           path to save file data to
+
+  optional arguments:
+    -h, --help            show this help message and exit
+    --metadata_url METADATA_URL
+                          ENCODE metadata URL.
+    --min_chip_per_cell MIN_CHIP_PER_CELL
+                          Minimum ChIP-seq experiments for each cell type.
+    --min_cells_per_chip MIN_CELLS_PER_CHIP
+                          Minimum cells a given ChIP-seq target must be observed
+                          in.
+    --bigBedToBed BIGBEDTOBED
+                          Path to bigBedToBed executable, downloaded from
+                          http://hgdownload.cse.ucsc.edu/admin/exe/
+
+
+To use your own dataset in an Epitome model, make sure to set the environment environment variable
+``EPITOME_DATA_PATH`` that points to your custom dataset. This will tell Epitome where to load
+data from.
+
+.. code:: bash
+
+  import os
+  os.environ["EPITOME_DATA_PATH"] = 'path/to/my/epitome/dataset'
+  ...
+
+
+Generating data for Epitome from ChIP-Atlas
+-------------------------------------------
+
+.. code:: bash
+
+  >> python download_chip_atlas.py -h
+
+  usage: download_chip_atlas.py [-h] [--metadata_url METADATA_URL]
+                          [--min_chip_per_cell MIN_CHIP_PER_CELL]
+                          [--min_cells_per_chip MIN_CELLS_PER_CHIP]
+                          download_path
+                          {ce10,ce11,dm3,dm6,hg19,hg38,mm10,mm9,rn6,sacCer3}
+                          output_path
+
+  Downloads ChIP-Atlas data from a chip_atlas_experiment_list.csv file.
+
+  positional arguments:
+  download_path         Temporary path to download bed/bigbed files to.
+  {ce10,ce11,dm3,dm6,hg19,hg38,mm10,mm9,rn6,sacCer3}
+                        assembly to filter files in metadata.tsv file by.
+  output_path           path to save file data to
+
+  optional arguments:
+  -h, --help            show this help message and exit
+  --metadata_url METADATA_URL
+                        ChIP-Atlas metadata URL.
+  --min_chip_per_cell MIN_CHIP_PER_CELL
+                        Minimum ChIP-seq experiments for each cell type.
+  --min_cells_per_chip MIN_CELLS_PER_CHIP
+                        Minimum cells a given ChIP-seq target must be observed
+                        in.
+
+
+TODO: need to add this script as a binary in the module.
---
+++ epitome/conversion.py
@@ -0,0 +1,160 @@
+from .functions import bed2Pyranges
+import pyranges as pr
+import numpy as np
+
+class RegionConversion:
+    '''
+    Class for dealing with genomic region conversions. In Epitome,
+    we often have to join a user's query genomic regions with a larger
+    region set (i.e. EpitomeDataset regions). This class performs two functions:
+    converting a user defined list of genomic regions to a vector that matches
+    a larger bed file, and converting Epitome predictions back to the original bed
+    regions.
+
+    '''
+
+    def __init__(self, base_bed, compare_bed):
+        '''
+        __init__ function for RegionConversion class.
+
+        :param str|pyranges base_bed: either path to bed file or indexed pyranges base object
+        :param str|pyranges compare_bed: either path to bed file or indexed pyranges comparison object
+        '''
+
+        # load in bed files as pyranges with index column
+        # types should end up being pyranges
+        self.base = RegionConversion.convert(base_bed)
+        self.compare = RegionConversion.convert(compare_bed)
+        self.joined = self.compare.join(self.base, how='left',suffix='_base')
+
+    @staticmethod
+    def convert(regions):
+        '''
+        Converts a bed file to indexed pyranges. If already a pyranges object, returns.
+
+        :param str|pyranges regions: either path to bed file or indexed pyranges object
+        :return: indexed PyRanges
+        :rtype: pr.pyranges
+        '''
+
+        if type(regions) == str:
+            regions_bed = bed2Pyranges(regions)
+        elif type(regions) == pr.PyRanges:
+            if 'idx' not in regions.columns:
+                regions.idx = np.arange(0, len(regions))
+                print("Warning: regions pyranges must have column named 'idx'")
+            regions_bed = regions
+        else:
+            raise Exception("regions must be type scoring or pr.Pyranges, but got type %s" % type(regions))
+        return regions_bed
+
+
+    def _get_overlap(self):
+        '''
+        Returns locations where there is supporting data in
+        both base and compare
+
+        :return: pyranges object containing regions that have overlapping data
+        for both compare and base
+        :rtype: pr.pyranges
+        '''
+        return self.joined[self.joined.idx_base > -1]
+
+    def get_base_overlap_index(self):
+        '''
+        Returns index of locations in *base* where there is supporting data in
+        both base and compare
+        '''
+        return self._get_overlap().idx_base
+
+
+    def get_binary_vector(self, vector = None):
+        '''
+        Finds indices in base that overlap compare and return vector matching
+        regions.
+
+        :return: tuple of vector of len(base) with ones that match compare, and
+            indices in base that have data for compare
+        :rtype: tuple
+        '''
+
+        if vector is None:
+            vector = np.ones(len(self.compare))
+
+        assert len(vector.shape) == 1, "Error: value_vector must be a 1D array"
+        assert vector.shape[0] == len(self.compare), "Error: value_vector must be the same shape as self.compare"
+
+        base_vector = np.zeros((len(self.base)))
+        print(base_vector.shape)
+
+        tmp = self._get_overlap()
+        print(tmp)
+
+        base_indices = tmp.idx_base.values
+        convert_indices = tmp.idx.values
+
+        print("bv",base_vector[base_indices])
+        print("v",vector[convert_indices])
+
+        base_vector[base_indices] = vector[convert_indices]
+
+        return base_vector, base_indices
+
+    def compare_df(self):
+        ''' Gets genomic regions ordered by idx '''
+        return self.compare.df.sort_values(by='idx').drop(labels='idx', axis=1)
+
+    def merge(self, matrix, axis = -2):
+        '''
+        Groups matrix shaped by base pyranges and calculates new predictions by
+        taking the mean to match the shape of compare pyranges.
+        This is for the case when you are predicting in regions
+        that may overlap multiple regions in an EpitomeDataset.
+
+        :param np.matrix matrix: matrix, where exactly 1 axis matches the shape of joined base/compare regions
+        :param int axis: axis that represents genomic regions. Defaults to -2.
+
+        :return: reduced np.matrix, axis specified is reduced
+        :rtype: np.matrix
+        '''
+
+        joined = self._get_overlap() \
+            .df \
+            .sort_values(by='idx') \
+            .reset_index(drop=True)
+
+        assert matrix.shape[axis] == len(joined), """joined and matrix must have the same dimension,
+            but got %i and %i """ % (matrix.shape[axis],len(joined))
+
+        # get the index break for each region_bed region
+        deduped = joined.drop_duplicates('idx',keep='first')
+        reduce_indices = deduped.index.values
+
+        # get the number of times there was a scored region for each region_bed region
+        # used to calculate reduced means
+        indices_counts = joined['idx'].value_counts(sort=False).sort_index().values
+
+
+        # define shape for results. make sure regions axis is length of compare.
+        shape = list(matrix.shape)
+        shape[axis] = len(self.compare)
+        final = np.empty((shape))
+        final[:] = np.nan
+
+        for i in range(len(matrix.shape)):
+            if i != axis:
+                indices_counts = np.expand_dims(indices_counts, axis=i)
+
+        reduced_values = np.add.reduceat(matrix, reduce_indices, axis = axis)/indices_counts
+
+        # get index to select and expand dimensions to match final
+        index = deduped['idx'].values
+
+        for i in range(len(final.shape)):
+            if i != axis:
+                index = np.expand_dims(index, axis=i)
+
+        # put reduced slices in correct indices
+        np.put_along_axis(final, index, reduced_values, axis)
+
+        return final
